31c8f202f59050353995793222110c2fe8a01a60
soncreux <98056199+soncreux@users.noreply.github.com>
2023-09-27 14:02:18 +0300

    Add files via upload

---
+++ palindrome.js
@@ -0,0 +1,15 @@
+function checkPalindrome(string) {
+
+    const arrayValues = string.split('');
+
+    const reverseArrayValues = arrayValues.reverse();
+
+    const reverseString = reverseArrayValues.join('');
+
+    if(string == reverseString) {
+        console.log('It is a palindrome');
+    }
+    else {
+        console.log('It is not a palindrome');
+    }
+}
